87b303a49c07473c5a06d5d37fa0eb5759bc982f
Daddy2054 <stivali-tinte-0w@icloud.com>
2024-11-30 21:26:29 +0100

    Initial Commit

---
+++ mygrocerylist/mygrocerylistApp.swift
@@ -0,0 +1,17 @@
+//
+//  mygrocerylistApp.swift
+//  mygrocerylist
+//
+//  Created by Jean on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct mygrocerylistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
